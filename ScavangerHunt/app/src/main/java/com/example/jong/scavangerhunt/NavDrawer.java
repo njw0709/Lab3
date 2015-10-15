4d1cb7dccae0558f1e2aa957536e50859e0f4299
Bill Wong <billmwong@gmail.com>
2015-10-15 19:46:24 -0400

    Working navigation drawer

---
+++ ScavangerHunt/app/src/main/java/com/example/jong/scavangerhunt/NavDrawer.java
@@ -0,0 +1,174 @@
+package com.example.jong.scavangerhunt;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class NavDrawer extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    static final LatLng TutorialsPoint = new LatLng(21 , 57);
+    private GoogleMap googleMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav_drawer);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        final Button myButton2 = (Button) findViewById(R.id.button);
+        myButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myButton2.setText("pushed button 2");
+            }
+        });
+
+        // Google Maps stuff
+        try {
+            if (googleMap == null) {
+//                googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+                googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            }
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            Marker TP = googleMap.addMarker(new MarkerOptions().
+                    position(TutorialsPoint).title("TutorialsPoint"));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Add a marker in Olin and move the camera
+//        LatLng olin = new LatLng(42.293194444444445, -71.26316666666666);
+        // Wait for map to not be null
+//        while (googleMap == null){}
+//        LatLng olin = new LatLng(42, -71);
+//        googleMap.addMarker(new MarkerOptions().position(olin).title("This is Olin"));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(olin));
+
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+//    public void onMapReady(GoogleMap mMap) {
+//        googleMap = mMap;
+//
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.nav_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+//        if (id == R.id.nav_camara) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
+
+//public class MainActivity extends Activity {
+//    static final LatLng TutorialsPoint = new LatLng(21 , 57);
+//    private GoogleMap googleMap;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        try {
+//            if (googleMap == null) {
+//                googleMap = ((MapFragment) getFragmentManager().
+//                        findFragmentById(R.id.map)).getMap();
+//            }
+//            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+//            Marker TP = googleMap.addMarker(new MarkerOptions().
+//                    position(TutorialsPoint).title("TutorialsPoint"));
+//        }
+//        catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
